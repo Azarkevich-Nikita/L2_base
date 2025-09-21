91e3f2567ebc139433db13c389410ba8e935f8de
Azarkevich-Nikita <mykesvek@gmail.com>
2025-09-21 19:30:31 +0300

    feat: Add Task17

---
+++ Task17.js
@@ -0,0 +1,12 @@
+//https://www.codewars.com/kata/525481903700c1a1ff0000e1
+
+function cache(func) {
+  var calls = {};
+  return function() {
+    var key = JSON.stringify(arguments);
+    if (!(key in calls)) {
+      calls[key] = func.apply(null, arguments);
+    }
+    return calls[key];
+  };
+}
